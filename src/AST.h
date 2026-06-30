66c0760d932b788aee0dfc164d9fd453a60b52a1
fuquan99666 <1924293797@qq.com>
2026-06-30 16:10:21 +0800

    hello.c AST

--- src/AST.h
+++ src/AST.h
@@ -1,47 +1,79 @@
+#pragma once
 #include <memory>
 #include <string>
+#include <iostream>
 
 // 所有 AST 的 基类
 class BaseAST {
     public:
         virtual ~BaseAST() = default;
+
+        virtual void Dump() const = 0;
 };
 
 
 // CompUnit 是 BaseAST 
-class CompUnit : public BaseAST {
+class CompUnitAST : public BaseAST {
     public:
     // 用智能指针管理对象
     std::unique_ptr<BaseAST> func_def;
+
+    void Dump() const override {
+        std::cout << "CompUnitAST { ";
+        func_def->Dump();
+        std::cout << " }" ;
+    }
 };
 
 
 // FuncDef 也是 BaseAST
-class FuncDef : public BaseAST {
+class FuncDefAST : public BaseAST {
     public:
      std::unique_ptr<BaseAST> func_type;
      std::string ident;
      std::unique_ptr<BaseAST> block;
+
+     void Dump() const override {
+        std::cout << "FuncDefAST { ";
+        func_type->Dump();
+        std::cout << ", " << ident << ", ";
+        block->Dump();
+        std::cout << " }" ;
+     }
 };
 
 // FuncType 也是 BaseAST
-class FuncType : public BaseAST {
+class FuncTypeAST : public BaseAST {
     public:
      std::string type;
+
+     void Dump() const override {
+        std::cout << "FuncTypeAST { " << type << " }" ;
+     }
 };
 
 
 // Block 也是 BaseAST
-class Block : public BaseAST {
+class BlockAST : public BaseAST {
     public:
      std::unique_ptr<BaseAST> stmt;
+
+     void Dump() const override {
+        std::cout << "BlockAST { ";
+        stmt->Dump();
+        std::cout << " }" ;
+     }
 };
 
 
 // Stmt 也是 BaseAST
-class Stmt : public BaseAST {
+class StmtAST : public BaseAST {
     public:
      int number;
+
+     void Dump() const override {
+        std::cout << "StmtAST { return " << number << "; }" ;
+     }
 };
 
 
